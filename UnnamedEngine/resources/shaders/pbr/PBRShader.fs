b3c1ea08d21c9b2cbcab1e147c4905bbc7c41db7
2851999 <2851999@users.noreply.github.com>
2018-04-05 12:27:19 +0100

    Release 1 | Beta 0 | Development 4 | Experimental 8
    * Cleaned up PBR shaders a bit

--- UnnamedEngine/resources/shaders/pbr/PBRShader.fs
+++ UnnamedEngine/resources/shaders/pbr/PBRShader.fs
@@ -1,5 +1,7 @@
 #include "../lighting/Lighting.fs"
 
+#include "PBRCore.fs"
+
 #map uniform IrradianceMap irradianceMap
 #map uniform PrefilterMap prefilterMap
 #map uniform BRDFLUT brdfLUT
@@ -9,49 +11,6 @@ uniform samplerCube prefilterMap;
 uniform sampler2D   brdfLUT; 
 
 out vec4 ue_FragColour;
-
-const float PI = 3.14159265359;
-
-//Returns ratio of light that gets reflected on a surface (k_S)
-vec3 fresnelSchlick(float cosTheta, vec3 F0) {
-	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
-vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness) {
-	return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
-float distributionGGX(vec3 N, vec3 H, float roughness) {
-	float a = roughness * roughness;
-	float a2 = a * a;
-	float NdotH = max(dot(N, H), 0.0);
-	float NdotH2 = NdotH * NdotH;
-
-	float num = a2;
-	float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-	denom = PI * denom * denom;
-
-	return num / denom;
-}
-
-float geometrySchlickGGX(float NdotV, float roughness) {
-	float r = (roughness + 1.0);
-	float k = (r * r) / 8.0;
-
-	float num = NdotV;
-	float denom = NdotV * (1.0 - k) + k;
-
-	return num / denom;
-}
-
-float geometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
-	float NdotV = max(dot(N, V), 0.0);
-	float NdotL = max(dot(N, L), 0.0);
-	float ggx2 = geometrySchlickGGX(NdotV, roughness);
-	float ggx1 = geometrySchlickGGX(NdotL, roughness);
-
-	return ggx1 * ggx2;
-}
 
 void main() {
 	vec3 lightPosition = vec3(0.5, 2.0, 2.0);
--- UnnamedEngine/resources/shaders/pbr/Prefilter.fs
+++ UnnamedEngine/resources/shaders/pbr/Prefilter.fs
@@ -1,5 +1,7 @@
 #version 330
 
+#include "PBRCore.fs"
+
 out vec4 FragColor;
 
 in vec3 localPos;
@@ -11,57 +13,6 @@ in vec3 localPos;
 uniform samplerCube environmentMap;
 uniform float envMapSize;
 uniform float roughness;
-
-const float PI = 3.14159265359;
-
-//Wont work on all drivers
-float radicalInverse_VdC(uint bits) {
-    bits = (bits << 16u) | (bits >> 16u);
-    bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
-    bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
-    bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
-    bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
-    return float(bits) * 2.3283064365386963e-10;
-}
-
-vec2 hammersley(uint i, uint N) {
-	return vec2(float(i) / float(N), radicalInverse_VdC(i));
-}
-
-vec3 importanceSampleGGX(vec2 Xi, vec3 N, float roughness) {
-    float a = roughness*roughness;
-	
-    float phi = 2.0 * PI * Xi.x;
-    float cosTheta = sqrt((1.0 - Xi.y) / (1.0 + (a*a - 1.0) * Xi.y));
-    float sinTheta = sqrt(1.0 - cosTheta*cosTheta);
-	
-    //Spherical coordinates to cartesian
-    vec3 H;
-    H.x = cos(phi) * sinTheta;
-    H.y = sin(phi) * sinTheta;
-    H.z = cosTheta;
-	
-    //Tangent-space vector to world-space sample vector
-    vec3 up        = abs(N.z) < 0.999 ? vec3(0.0, 0.0, 1.0) : vec3(1.0, 0.0, 0.0);
-    vec3 tangent   = normalize(cross(up, N));
-    vec3 bitangent = cross(N, tangent);
-	
-    vec3 sampleVec = tangent * H.x + bitangent * H.y + N * H.z;
-    return normalize(sampleVec);
-}
-/* ------------------------- COPIED FROM PBR SHADER ------------------------- */
-float distributionGGX(vec3 N, vec3 H, float roughness) {
-	float a = roughness * roughness;
-	float a2 = a * a;
-	float NdotH = max(dot(N, H), 0.0);
-	float NdotH2 = NdotH * NdotH;
-
-	float num = a2;
-	float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-	denom = PI * denom * denom;
-
-	return num / denom;
-}
 
 void main() {
     const uint SAMPLE_COUNT = 1024u;
